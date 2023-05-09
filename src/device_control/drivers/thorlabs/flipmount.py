7325cf11beba2aa762c1c1b6fa0813b01d97c64b
Miles Lucas <mdlucas@hawaii.edu>
2023-05-09 10:40:28 +0000

    add configuration to flippers

--- src/device_control/drivers/thorlabs/flipmount.py
+++ src/device_control/drivers/thorlabs/flipmount.py
@@ -1,5 +1,6 @@
 from serial import Serial
 import tomli
+import numpy as np
 
 
 # Raw byte commands for "MGMSG_MOT_MOVE_JOG"
@@ -16,10 +17,11 @@ STATUSES = {
 
 
 class ThorlabsFlipMount:
-    def __init__(self, address, name="", config_file=None, **kwargs):
+    def __init__(self, address, configurations=None, name="", config_file=None, **kwargs):
         self._address = address
         self._name = name
         self.config_file = config_file
+        self._configurations = configurations
         self.serial = Serial(
             port=self.address,
             baudrate=115200,
@@ -43,35 +45,63 @@ class ThorlabsFlipMount:
     def address(self, value: str):
         self._address = value
 
+    @property
+    def configurations(self):
+        return self._configurations
+
+    @configurations.setter
+    def configurations(self, value):
+        self._configurations = value
+
     @classmethod
     def from_config(__cls__, filename):
         with open(filename, "rb") as fh:
             parameters = tomli.load(fh)
-        return __cls__(config_file=filename, **parameters)
+        configurations = parameters.pop("configurations", None)
+
+        return __cls__(configurations=configurations, config_file=filename, **parameters)
 
     def load_config(self, filename=None):
         if filename is None:
             filename = self.config_file
         with open(filename, "rb") as fh:
             parameters = tomli.load(fh)
-        self.address = parameters["address"]
-        self.name = parameters["name"]
-        self.config_file = filename
+        self._config_file = filename
+        self._address = parameters["address"]
+        self._name = parameters.get("name", "")
+        self._configurations = parameters.get("configurations", None)
+
+    def move_configuration(self, name: str):
+        for row in self.configurations:
+            if row["name"].lower() == name.lower():
+                return self.flip(row["idx"])
+        raise ValueError(f"No configuration saved with name '{name}'")
 
     def flip(self, position: str):
-        if position.lower() == "in":
+        if position.lower() == "down":
             cmd = COMMANDS["down"]
-        elif position.lower() == "out":
+        elif position.lower() == "up":
             cmd = COMMANDS["up"]
+        else:
+            raise ValueError(f"Position should be either 'up' or 'down', got '{position}'")
+
         with self.serial as serial:
             serial.write(cmd)
-
-    def status(self):
+    
+    @property
+    def position(self):
         with self.serial as serial:
             serial.write(COMMANDS["status"])
             result = serial.read(12)
-            if result == STATUSES["down"]:
-                return "IN"
-            elif result == STATUSES["up"]:
-                return "OUT"
-            return "Unknown"
+        if result == STATUSES["down"]:
+            return "DOWN"
+        elif result == STATUSES["up"]:
+            return "UP"
+        return "Unknown"
+
+    def get_configuration(self):
+        position = self.position
+        for row in self.configurations:
+            if row["idx"].lower() == position.lower():
+                return row["idx"], row["name"]
+        return None, "Unknown"
