feca53e7810b1679ea15c0412fdaf4ce3edfd6c5
Miles Lucas <mdlucas@hawaii.edu>
2023-05-17 13:55:20 -1000

    update arduino trigger code

--- src/device_control/vampires/vampires_trigger.py
+++ src/device_control/vampires/vampires_trigger.py
@@ -3,6 +3,7 @@ from typing import Union
 import numpy as np
 from serial import Serial
 from dataclasses import dataclass
+import astropy.units as u
 
 # from swmain.redis import update_keys
 
@@ -10,7 +11,11 @@ from dataclasses import dataclass
 @dataclass
 class VAMPIRESTrigger:
     address: str
-    timing_info: dict
+    tint: Union[float, int, u.Quantity] = 2 * u.ms
+    pulse_width: int = 10 # us
+    flc_offset: int = 20 # us
+    flc_enabled: bool = True
+    sweep_mode: bool = False
     config_file=None
 
     def __post_init__(self, **kwargs):
@@ -32,24 +37,40 @@ class VAMPIRESTrigger:
     def get_timing_info(self):
         resp = self.ask_command(0)
         tokens = resp.split()
+        enabled = bool(tokens[0])
+        self.tint = int(tokens[1])
+        self.pulse_width = int(tokens[2])
+        self.flc_offset = int(tokens[3])
+        trigger_mode = int(tokens[4])
+        self.flc_enabled = trigger_mode & 0x1
+        self.sweep_mode = trigger_mode & 0x2
         return {
-            "enabled": bool(tokens[0]),
-            "integration_time": int(tokens[1]),
-            "pulse_width": int(tokens[2]),
-            "flc_offset": int(tokens[3]),
-            "trigger_mode": int(tokens[4]),
+            "enabled": enabled,
+            "integration_time": self.tint,
+            "pulse_width": self.pulse_width,
+            "flc_offset": self.flc_offset,
+            "flc_enabled": self.flc_enabled,
+            "sweep_mode": self.sweep_mode,
         }
 
-    def set_timing_info(self, timing_info=None):
-        if timing_info is None:
-            timing_info = self.timing_info
-        cmd = "1 {:d} {:d} {:d} {:d}".format(
-            timing_info["integration_time"],
-            timing_info["pulse_width"],
-            timing_info["flc_offset"],
-            timing_info["trigger_mode"],
+    def set_timing_info(self):
+        trigger_mode = int(self.flc_enabled) + (int(self.sweep_mode) << 1)
+        cmd = "1 {:.0f} {:.0f} {:.0f} {:.0f}".format(
+            self.tint,
+            self.pulse_width,
+            self.flc_offset,
+            trigger_mode
         )
         self.send_command(cmd)
+
+    def set_tint(self, tint):
+        if isinstance(tint, u.Quantity):
+            tint_us = tint.to(u.us).value
+        else:
+            # tint in ms to us
+            tint_us = int(tint * 1e3)
+        self.tint = tint_us
+        self.set_timing_info()
 
     def disable(self):
         self.send_command(2)
@@ -63,23 +84,14 @@ class VAMPIRESTrigger:
         #     U_FLCTM=self.timing_info["pulse_width"]
         # )
 
-    def disable_flc(self):
-        self.send_command(4)
-        # update_keys(U_FLCEN="OFF")
-
-    def enable_flc(self):
-        self.send_command(5)
-        # update_keys(U_FLCEN="ON")
-
     @classmethod
     def from_config(__cls__, filename):
         with open(filename, "rb") as fh:
             parameters = tomli.load(fh)
 
         return __cls__(
-            address=parameters["address"],
-            timing_info=parameters["timing"],
-            config_file=filename
+            config_file=filename,
+            **parameters,
         )
 
     def load_config(self, filename=None):
@@ -89,7 +101,8 @@ class VAMPIRESTrigger:
             parameters = tomli.load(fh)
         
         self.address = parameters["address"]
-        self.timing_info = parameters["timing"]
+        self.pulse_width = parameters.get("pulse_width", self.pulse_width)
+        self.flc_offset = parameters.get("flc_offset", self.flc_offset)
         self.config_file = filename
 
     def status(self):
