dcb134a5d2f6296d7f45aed1a9822014d562e685
Miles Lucas <mdlucas@hawaii.edu>
2023-05-17 13:55:20 -1000

    update arduino trigger code

--- src/arduino/vampires_trigger/vampires_trigger.ino
+++ src/arduino/vampires_trigger/vampires_trigger.ino
@@ -1,17 +1,22 @@
+#include <Adafruit_NeoPixel.h>
+
 // Here's some constants up front for easy retrieval
 #define BAUDRATE 115200
 #define TIMEOUT 500 // ms
 // Pin mapping
-#define CAMERA_TRIGGER_PIN 2
 #define FLC_CONTROL_PIN 4
 #define FLC_TRIGGER_PIN 6
+#define CAMERA_TRIGGER_PIN 8
 #define AUX_PIN_A -1 // unused dig I/O pin
 #define AUX_PIN_B -1 // unused dig I/O pin
 #define AUX_PIN_C -1 // unused dig I/O pin
+#define NEOPIXEL_PIN 40
+
+Adafruit_NeoPixel strip(1, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 // FYI C people: Arduino int is 16bit, long is 32 bits.
 // We use unsigned long for everything time-related in microseconds.
-const unsigned long max_integration_time = 1600000000;
+const unsigned long max_integration_time = 1600000000; // us
 const unsigned long max_flc_integration_time = 1000000;
 const unsigned long min_integration_time = 8; // 7.2 us line read time
 unsigned long last_loop_finish;
@@ -26,29 +31,37 @@ unsigned long dt6;
 unsigned long integration_time;
 unsigned long pulse_width;
 unsigned long flc_offset;
-int trigger_mode;
-int cmd_code;
+unsigned int trigger_mode;
+unsigned int cmd_code;
 
 
-int sweep_mode = 0;
-int auto_reset_mode = 0;
-unsigned long next_reset = 0;
-bool loop_enabled = false;
+unsigned int sweep_mode;
+unsigned long next_reset;
+bool loop_enabled;
 
 void setup()
-{
+{    // variable resets
+    sweep_mode = next_reset = 0;
+    loop_enabled = false;
+    integration_time = min_integration_time;
+    pulse_width = 10;
+    flc_offset = 20;
+
     // set pin modes to output
     pinMode(CAMERA_TRIGGER_PIN, OUTPUT);
     pinMode(FLC_TRIGGER_PIN, OUTPUT);
     pinMode(FLC_CONTROL_PIN, OUTPUT);
     pinMode(13, OUTPUT);
-    digitalWrite(13, loop_enabled);
-
 
     // reset outputs to low
     digitalWrite(CAMERA_TRIGGER_PIN, LOW);
     digitalWrite(FLC_TRIGGER_PIN, LOW);
     digitalWrite(FLC_CONTROL_PIN, LOW);
+
+    // neopixel setup
+    strip.begin();
+    strip.setPixelColor(0, 100, 0, 0);
+    strip.show();
 
     // start serial connection
     Serial.setTimeout(TIMEOUT);
@@ -77,7 +90,7 @@ void set(int _integration_time, int _pulse_width, int _flc_offset, int _trigger_
     }
     else if (_flc_offset < 0 || _flc_offset + _pulse_width >= _integration_time)
     {
-        Serial.println("ERROR - invalid FLC offset must be positive and < width+period");
+        Serial.println("ERROR - invalid FLC offset: must be positive and < width+period");
         return;
     }
     // set global variables
@@ -85,8 +98,11 @@ void set(int _integration_time, int _pulse_width, int _flc_offset, int _trigger_
     pulse_width = _pulse_width;
     flc_offset = _flc_offset;
     trigger_mode = _trigger_mode;
+    digitalWrite(FLC_CONTROL_PIN, trigger_mode & 0x1);
+    digitalWrite(13, trigger_mode & 0x1);
+
     // Use LSB for sweep
-    if (trigger_mode & 0x1)
+    if (trigger_mode & 0x2)
     {
         sweep_mode = 1;
     }
@@ -109,11 +125,11 @@ void prepareLoop() {
     // Compute values for loop
     dt1 = flc_offset;
     dt2 = dt1 + pulse_width;
-    dt3 = integration_time;
+    dt3 = dt1 + integration_time;
     // Asymmetric triggering by triggering the second frame 20 us too late (see below)
-    dt4 = dt1 + integration_time + 20; // 20 us for measurable FLC on / FLC off skew
-    dt5 = dt2 + integration_time + 20;
-    dt6 = dt3 + integration_time;
+    dt4 = dt3 + flc_offset; // 20 us for measurable FLC on / FLC off skew
+    dt5 = dt4 + pulse_width;
+    dt6 = dt4 + integration_time;
     // reset timer
     last_reset = micros();
     // last_loop_finish is really when the loop last finished.
@@ -135,19 +151,15 @@ void loop()
                 prepareLoop();
                 break;
             case 2: // DISABLE
-                loop_enabled = false;
-                digitalWrite(13, LOW);
+                loop_enabled = false;    
+                strip.setPixelColor(0, 100, 0, 0);
+                strip.show();
                 break;
             case 3: // ENABLE
                 prepareLoop();
                 loop_enabled = true;
-                digitalWrite(13, HIGH);
-                break;
-            case 4: // FLC_DISABLE
-                digitalWrite(FLC_CONTROL_PIN, LOW);
-                break;
-            case 5: // FLC_ENABLE
-                digitalWrite(FLC_CONTROL_PIN, HIGH);
+                strip.setPixelColor(0, 0, 100, 0);
+                strip.show();
                 break;
             default:
                 Serial.println("ERROR - invalid command");
@@ -160,6 +172,7 @@ void loop()
     if (loop_enabled)
     {
         // First pola flc_pin LOW
+        digitalWrite(FLC_TRIGGER_PIN, LOW);
         for (; micros() - last_loop_finish < dt1;) {} // This way of testing works around micros() overflowing
         digitalWrite(CAMERA_TRIGGER_PIN, HIGH);
         for (; micros() - last_loop_finish < dt2;) {}
@@ -184,16 +197,6 @@ void loop()
                 flc_offset = 0;
             }
         }
-
-        if (auto_reset_mode && (micros() - last_reset) >= 5000000) // 5 sec
-        {
-            // Sleep for 150 ms
-            // EDT framegrabber set to timeout at 100 ms. Force it to timeout.
-            for (; micros() - last_loop_finish < dt6 + 150000;) {}
-            // Next reset in 5 sec.
-            last_reset = micros();
-            last_loop_finish = last_reset;
-        } // if auto_reset
 
         last_loop_finish += dt6; // We take it that way, rather than calling micros() again.
                                      // Otherwise the loop will be a teensy tiny bit longer than 2*integration_time.
