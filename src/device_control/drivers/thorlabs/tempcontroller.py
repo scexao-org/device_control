768f0d18eca54860cb35248a7602508ffc53af4b
Miles Lucas <mdlucas@hawaii.edu>
2023-05-18 08:28:38 +0000

    update temp controller to avoid properties

--- src/device_control/drivers/thorlabs/tempcontroller.py
+++ src/device_control/drivers/thorlabs/tempcontroller.py
@@ -1,5 +1,6 @@
 from serial import Serial
 import tomli
+from device_control.base import ConfigurableDevice
 
 
 def parse_status(bytevalues):
@@ -19,48 +20,11 @@ def parse_status(bytevalues):
     return output
 
 
-class ThorlabsTC:
-    def __init__(self, address, name="", config_file=None, **kwargs):
-        self._address = address
-        self._name = name
-        self.config_file = config_file
-        self.serial = Serial(
-            port=self.address,
-            baudrate=115200,
-            **kwargs,
-        )
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value: str):
-        self._name = value
-
-    @property
-    def address(self):
-        return self._address
-
-    @address.setter
-    def address(self, value: str):
-        self._address = value
-
-    @classmethod
-    def from_config(__cls__, filename):
-        with open(filename, "rb") as fh:
-            parameters = tomli.load(fh)
-        return __cls__(config_file=filename, **parameters)
-
-    def load_config(self, filename=None):
-        if filename is None:
-            filename = self.config_file
-        with open(filename, "rb") as fh:
-            parameters = tomli.load(fh)
-        self.address = parameters["address"]
-        self.name = parameters["name"]
-        self.unit = parameters.get("unit", None)
-        self.config_file = filename
+class ThorlabsTC(ConfigurableDevice):
+    def __init__(self, serial_kwargs, temp, **kwargs):
+        serial_kwargs = dict({"baudrate": 115200}, **serial_kwargs)
+        super().__init__(serial_kwargs=serial_kwargs, **kwargs)
+        self.set_target(temp)
 
     def send_command(self, cmd: str):
         with self.serial as serial:
@@ -73,17 +37,14 @@ class ThorlabsTC:
             serial.read_until(b"\r")
             return serial.read_until(b"\r").decode().strip()
 
-    @property
-    def target(self):
+    def get_target(self):
         result = self.ask_command("tset?")
         return float(result.split()[0])
 
-    @target.setter
-    def target(self, value: float):
+    def set_target(self, value: float):
         self.send_command(f"tset={value:.01f}")
 
-    @property
-    def temp(self):
+    def get_temp(self):
         result = self.ask_command("tact?")
         return float(result.split()[0])
 
@@ -94,10 +55,8 @@ class ThorlabsTC:
             result = serial.read(2)
         return parse_status(result)
 
-    @property
-    def id(self):
-        result = self.ask_command("*idn?")
-        return result
+    def get_id(self):
+        return self.ask_command("*idn?")
 
     def enable(self):
         status = self.status()
